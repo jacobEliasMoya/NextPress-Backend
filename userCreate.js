f8d4ee0c4ea67d538a0ee2714e21d980aa194960
jacob moya <jemoya1994@Gmail.com>
2025-06-27 00:21:29 -0700

    working on implemnting proper headers to allow for CORS/ Options re the application/ JSON object

--- userCreate.js
+++ userCreate.js
@@ -1,9 +1,24 @@
-const http = require("node:http");
+import { createServer } from "node:http";
 const port = 5000;
 const host = "localhost";
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   const { method, url } = req;
+
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (method.toLowerCase() === "options") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 
   if (method.toLowerCase() === "post" && url === "/") {
     res.statusCode = 200;
